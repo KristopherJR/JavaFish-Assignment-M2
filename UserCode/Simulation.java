610cf6c474aa7ac7e8942198e6d156fe09a430b2
KristopherJR <k.randle@lancaster.ac.uk>
2020-12-16 23:41:30 +0000

    4 JavaFish are now added to the scene on start-up with Random scales, speeds and positions. Version 16-12-2020 V2.1

--- UserCode/Simulation.java
+++ UserCode/Simulation.java
@@ -28,6 +28,9 @@ public class Simulation
     
     // DECLARE a reference to the instance of 'List<IDisplayObject>', call it '_displayObjects'. Used to store all visual objects:
     private List<IDisplayObject> _displayObjects;
+    
+    // DECLARE and INITIALISE an int specifying the number of JavaFish to be added to the scene:
+    private int jNumber = 4;
 
     public static void main(String[]args)
     {
@@ -66,11 +69,11 @@ public class Simulation
      */    
     public void populateObjectArray()
     {
-        //INITIALISE the objects cast to type IDisplayObject:
-        IDisplayObject jf1 = new JavaFish(); //Create a 'JavaFish' called 'jf1', store it as an 'IDisplayObject'. Give it the specified scale.
-            
-        //STORE the newly initalized objects in the _displayObjects List.
-        _displayObjects.add(jf1); //add jf1 to the _displayObjects List.
+        //CREATE a specific number of JavaFish in a for loop using jNumber. Add them to the _displayObjects List.
+        for(int i = 0; i < jNumber; i++)
+        {
+            _displayObjects.add(new JavaFish());
+        }
     }
 
     /**
